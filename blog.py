ab01c5a63ff129ef6c631c5e4bb107acea3972f9
Masayaelvin <masayaelvin@gmail.com>
2023-11-03 14:43:48 +0300

    css_update

--- blog.py
+++ blog.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 posts = [
     {
-        'author': 'Corey Schafer',
+        'author': 'Wayne wonder Asamba',
         'title': 'Blog Post 1',
         'content': 'First post content',
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'Jane Doe',
+        'author': 'Masaya Elvin',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
