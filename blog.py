a5416beb11cd79ea093260bda6016b7167775dd8
Masayaelvin <masayaelvin@gmail.com>
2023-11-03 15:11:57 +0300

    forms

--- blog.py
+++ blog.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'b7f8824fbd4c68a1943bffeebd6c92cf'
 posts = [
     {
         'author': 'Wayne wonder Asamba',
@@ -22,6 +24,16 @@ def hello():
 @app.route('/about')
 def about():
     return render_template('about.html' , title='About')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form= RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/login')
+def login():
+    form= LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
