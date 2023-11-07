df506cc83d8c7b7b5e0ec471cec5b923651dd783
Masayaelvin <masayaelvin@gmail.com>
2023-11-07 10:56:31 +0300

    image_resize

--- f_blog/routes.py
+++ f_blog/routes.py
@@ -1,4 +1,5 @@
 import os
+from PIL import Image
 from flask import render_template, flash, url_for, redirect, request
 from f_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from f_blog.models import User, Post
@@ -67,7 +68,12 @@ def save_picture(form_picture):
       _ , f_ext = os.path.splitext(form_picture.filename)
       picture_fn = random_hex + f_ext
       picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-      form_picture.save(picture_path)
+
+      output_size = (125, 125)
+      i = Image.open(form_picture)
+      i.thumbnail(output_size)
+      i.save(picture_path)
+      
       return picture_fn
 
 @app.route("/account", methods=['POST', 'GET'])
