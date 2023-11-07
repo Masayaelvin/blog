5df78bf352ab86893d39663e54b1df4e7353a455
Masayaelvin <masayaelvin@gmail.com>
2023-11-07 09:53:41 +0300

    account_info

--- f_blog/forms.py
+++ f_blog/forms.py
@@ -1,7 +1,9 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, ValidationError
 from wtforms.validators import DataRequired , Length, Email, EqualTo
 from f_blog.models import User
+
 
 class RegistrationForm(FlaskForm):
     username= StringField('Username', 
@@ -27,3 +29,22 @@ class LoginForm(FlaskForm):
     password= PasswordField('Password', validators=[DataRequired()])
     remember= BooleanField('Remember Me')
     submit= SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username= StringField('Username', 
+                          validators=[DataRequired(), Length(min=2, max=20)])
+    email= StringField('Email', validators=[DataRequired(), Email()])
+    submit= SubmitField('update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('username already exists choose a different one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email already exists choose a different one')
--- f_blog/routes.py
+++ f_blog/routes.py
@@ -1,5 +1,5 @@
 from flask import render_template, flash, url_for, redirect, request
-from f_blog.forms import RegistrationForm, LoginForm
+from f_blog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from f_blog.models import User, Post
 from f_blog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
@@ -63,5 +63,7 @@ def logout():
 @app.route("/account")
 @login_required
 def account():
+      form = UpdateAccountForm()
       image_file = url_for("static", filename='profile_pics/' + current_user.image_file)
-      return render_template('account.html', title='Account', image_file = image_file)
+      return render_template('account.html', title='Account',
+                              image_file = image_file, form=form)
