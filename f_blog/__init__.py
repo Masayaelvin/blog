338ac6b4376d9b5bc82f569fd58213462288c41d
Masayaelvin <masayaelvin@gmail.com>
2023-11-11 09:42:26 +0300

    password_reset

--- f_blog/__init__.py
+++ f_blog/__init__.py
@@ -2,10 +2,19 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_mail import Mail
+import os
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = 'b7f8824fbd4c68a1943bffeebd6c92cf'
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = 'kerrystv14@gmail.com'
+app.config['MAIL_PASSWORD'] = 'lefc jlzp gfqv nzfz'
+
+mail = Mail(app)
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
